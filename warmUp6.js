60989fdb468c53470afa1a114a9b3adb5992010f
mohamedBenAmmar <mr.weezy1312@gmail.com>
2020-03-19 09:28:21 +0100

    warm up 6 done

--- warmUp6.js
+++ warmUp6.js
@@ -2,9 +2,27 @@
 // Read carefully, focus and test your code :) ... /\ HaPPy HaCkInG /\
 
 // 1-Define a function called myTrueFun that returns true whenever its called.
+function myTrueFun(){
+	return true ;
+}
 
 // 2- Define a function called color in which returns true if type of the input is string and returns false otherwise.
+function color(string){
+return (typeof string ==="string") ?  true :  false ;
+}
 
 // 3-Write a JavaScript function that accepts a number as a parameter and check if the number is prime or not.
+function prime (x){
+	var test=true;
+	for (var i=x-1; i>1 ;i--){
+		if (x%i===0) {
+		 test=false ;
+		}
+	}
+	return test;
+}
 
 // 4-write a function that accepts two numbers as parameters and returns true if the two parameters have the same value , false if not
+function compare (x,y){
+	return x===y ?  true : false ;
+}
