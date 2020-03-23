21703cff8afec454370ccf355ea5f00b0b7a4f5f
mohamedBenAmmar <mr.weezy1312@gmail.com>
2020-03-23 09:27:38 +0100

    warm up 9 done

--- warmUp9.js
+++ warmUp9.js
@@ -4,31 +4,27 @@
 //1. Write a JavaScript function to check whether an `input` is an array or not
 // ex isArray([1,2])=>true
 //    isArray(5)=>false
-
-// **************** git status ****************
-// **************** git add fileName.js ****************
-// **************** git status ****************
-// **************** git commit -m "your message" ****************
-// **************** git status ****************
-// **************** git push origin master ****************
+function checkArray (x) {
+	return x.isArray() ? true : false ;  
+}
 
 //2-Write a JavaScript function to get the first n element of an array.
 //ex  first([1,2,3],1)=>[1]
 //    first([1,2,3,4]2)=>[1,2]
 
-// **************** git status ****************
-// **************** git add fileName.js ****************
-// **************** git status ****************
-// **************** git commit -m "your message" ****************
-// **************** git status ****************
-// **************** git push origin master ****************
+function firstElement (x,n) {
+	for (var i=0 ; i < n ; i++){
+		console.log(x[i]);
+	}
+}
 
 //3- Write a JavaScript function to convert an object into a list of `[key, value]` pairs
 //ex: convert({name:'ahmed',age:20})=>[['name','ahmed'],['age', 20]]
 
-// **************** git status ****************
-// **************** git add fileName.js ****************
-// **************** git status ****************
-// **************** git commit -m "your message" ****************
-// **************** git status ****************
-// **************** git push origin master ****************
+function convert (x){
+	var y=[];
+	for (prop in x ){
+		y.push([x[prop],x["prop"]]);
+	}
+	return y;
+}
